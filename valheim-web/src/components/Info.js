621b0b573bee88dd60eba252bf2a078aae2a6251
DiegoG <80786405+Skapebreak@users.noreply.github.com>
2021-11-30 11:32:34 -0700

    Working on info page

--- valheim-web/src/components/Info.js
+++ valheim-web/src/components/Info.js
@@ -1,32 +1,47 @@
 import React from 'react'
 import '../views/Info.css'
 import Base1 from '../images/base1.jpg'
-import InfoBox from './InfoBox';
+
 
 
 function Info() {
     return (
         <div className="info">
-            <img clasName="" src={Base1} alt=""/>
-            
-      <div className="info__left">
-             
-            
-
-            <div>
-            {/* <h3>Hello, {user?.email}</h3> */}
-            <h2 className="info__title">What is BuildHeim?</h2>
-
-            <InfoBox />
+            <img clasName="info_image" src={Base1} alt=""/>   
+                
+            <div className='infobox'>
+                        
+                 <div className=''>
+                            
+                            <h2 className="info__title">
+                                What is BuildHeim?
+                            </h2>
+                                
+                            <h1 className='infobox__title'>
+                                Info
+                                asdasasdasdasdasdasdasfgsdgs
+                                gSDWGgdsgASDGgdG
+                                gGGgG
+                            </h1>
+                                
+                            <p className="infobox__price">
+                                hellosjkladhfgaskljdghsadlkjg
+                                sdghasdfjkghasjklghafsdjlkghdaf
+                                fakjhgsdfhjasdghjkasdgasdg
+                                asdgkjhdfgsjkghakljdfg
+                            </p>
+                            <div className="infobox__rating">
+                                    
+                                <p>🌟</p>
+                                    
+                            </div>
+                            
+                        
+                </div>
 
             </div>
-      </div>
-
-      {/* <div className="info__right">
-        
-      </div> */}
       
-    </div>
+        </div>
     )
 }
 
